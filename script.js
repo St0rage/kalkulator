665dc59b5ec50280fdb82ed2c11ed98af978b118
St0rage <daniyudistira25@gmail.com>
2020-12-27 15:59:11 +0700

    aplikasi sudah berjalan, tinggal menmabah funsgi hapus dan menambah README

--- script.js
+++ script.js
@@ -2,21 +2,96 @@ let tampung = [];
 let nilai1;
 let nilai2;
 let operator;
-tombolA = document.querySelectorAll('.tombolA');
-tombolO = document.querySelectorAll('.tombolO');
-samadengan  = document.querySelector('.samadengan');
+let tombolA = document.querySelectorAll('.tombolA');
+let tombolO = document.querySelectorAll('.tombolO');
+let outputs = document.querySelector('.output');
+let samadengan  = document.querySelector('.samadengan');
+
+// CLEAR
+document.querySelector('.clear').addEventListener('click', function() {
+	window.location.reload();
+});
 
 tombolA.forEach(function(el) {
 	el.addEventListener('click', function(e){
 		tampung.push(e.target.innerHTML);
+		let i = tampung.length - 1;
+		let output = document.createTextNode(tampung[i]);
+		outputs.appendChild(output);
 	});
 });
 
 tombolO.forEach(function(el) {
 	el.addEventListener('click', function(e) {
 		operator = e.target.innerHTML;
-		let tampungg = tampung.slice(0,14);
-		nilai1 = parseInt(tampungg.join(''));
+		nilai1 = parseFloat(tampung.join(''));
 		tampung = [];
-	})
+		outputs.innerHTML = '';
+		document.querySelector('.operator').innerHTML = operator;
+	});
 });
+
+samadengan.addEventListener('click', function() {
+	nilai2 = parseFloat(tampung.join(''));
+	tampung = [];
+	outputs.innerHTML = '';
+	document.querySelector('.operator').innerHTML = samadengan.innerHTML;
+
+		switch(operator) {
+			case "+":
+				tambah(operator, nilai1, nilai2);
+				break;
+			case "-":
+				kurang(operator, nilai1, nilai2);
+				break;
+			case "&#247":
+				bagi(operator, nilai1, nilai2);
+				break;
+			case "x":
+				kali(operator, nilai1, nilai2);
+		}
+});
+
+function tambah(operator, operand1, operand2) {
+	let hasil = operand1 + operand2;
+	if(hasil) {
+		outputs.innerHTML = hasil;
+	}else {
+		outputs.innerHTML = operand1;	
+	}
+	console.log(hasil);
+	return hasil;
+}
+
+function kurang(operator, operand1, operand2) {
+	let hasil = operand1 + operand2;
+	if(hasil) {
+		outputs.innerHTML = hasil;
+	}else {
+		outputs.innerHTML = operand1;	
+	}
+	console.log(hasil);
+	return hasil;
+}
+
+function bagi(operator, operand1, operand2) {
+	let hasil = operand1 + operand2;
+	if(hasil) {
+		outputs.innerHTML = hasil;
+	}else {
+		outputs.innerHTML = operand1;	
+	}
+	console.log(hasil);
+	return hasil;
+}
+
+function kali(operator, operand1, operand2) {
+	let hasil = operand1 + operand2;
+	if(hasil) {
+		outputs.innerHTML = hasil;
+	}else {
+		outputs.innerHTML = operand1;	
+	}
+	console.log(hasil);
+	return hasil;
+}
